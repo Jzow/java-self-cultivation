097d43c07e8f468cbd11a8f5fa2b90ca3bc8a4d6
JamesZow <Jameszow@163.com>
2021-03-26 22:04:12 +0800

    new add DeleteDuplicates.java

---
+++ LeetCode/src/main/java/com/jameszow/leetcode/easy/DeleteDuplicates.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2021 Apache All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+package com.jameszow.leetcode.easy;
+
+/**
+ * 删除排序链表中重复的元素
+ */
+public class DeleteDuplicates {
+
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null){
+            return head;
+        }
+        ListNode result = head;
+        while(result.next != null){
+            if(result.val == result.next.val){
+                result.next = result.next.next;
+            }else{
+                result = result.next;
+            }
+        }
+        return head;
+    }
+}
