1d4d5aab2d5c555127e016cef1e199857aa351e5
JamesZow <Jameszow@163.com>
2021-04-15 21:48:34 +0800

    new add Rob.java

---
+++ LeetCode/src/main/java/com/jameszow/leetcode/medium/Rob.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2021 Apache All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+package com.jameszow.leetcode.medium;
+
+import java.util.Arrays;
+
+/**
+ * 打家劫舍II
+ * 你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。
+ * 这个地方所有的房屋都 围成一圈 ，这意味着第一个房屋和最后一个房屋是紧挨着的。
+ * 同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警
+ * 给定一个代表每个房屋存放金额的非负整数数组，计算你 在不触动警报装置的情况下 ，能够偷窃到的最高金额
+ */
+public class Rob {
+
+    public static void main(String [] args){
+        int [] n = {2,3,2};
+        System.out.println(rob(n));
+    }
+
+    public static int rob(int [] nums){
+        if(nums.length == 0) return 0;
+        if(nums.length == 1) return nums[0];
+        return Math.max(myRob(Arrays.copyOfRange(nums, 0, nums.length -1)),
+                        myRob(Arrays.copyOfRange(nums, 1, nums.length)));
+    }
+
+    private static int myRob(int [] nums){
+        int pre = 0, cur = 0, temp;
+        for (int num : nums){
+            temp = cur;
+            cur = Math.max(pre + num , cur);
+            pre = temp;
+        }
+        return cur;
+    }
+}
