8ef7955d140b07735cdec08f9de0a0ea48d40cf6
James Zow <jameszow@163.com>
2022-09-15 08:52:40 +0800

    add observer design pattern example code

---
+++ DesignPattern/src/main/java/com/jameszow/observer/Subject.java
@@ -0,0 +1,14 @@
+package com.jameszow.observer;
+
+import java.util.Vector;
+
+/**
+ * 主题类
+ *
+ * @author James Zow
+ * @create 2022/9/14
+ */
+public abstract class Subject {
+
+    private String name;
+}
